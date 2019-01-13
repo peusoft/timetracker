affd71ab00e9f24cb7c02b7650297f49ff00eed6
Yauheni Prykhodzka <yauheni.prykhodzka@peusoft.com>
2019-01-13 18:59:18 +0100

    no db

--- src/main/java/com/peusoft/timetracker/Application.java
+++ src/main/java/com/peusoft/timetracker/Application.java
@@ -2,11 +2,13 @@ package com.peusoft.timetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
     @RequestMapping("/")
